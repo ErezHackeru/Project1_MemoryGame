c0acbd6c59832d7dba1de28d93e6d6e24aac07bc
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2018-12-09 23:05:43 +0200

    Full Project

--- MemoryGameProject.cs
+++ MemoryGameProject.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MemoryGame
 {
-    class Program
+    class MemoryGameProject
     {
         static void Main(string[] args)
         {
@@ -22,8 +22,9 @@ namespace MemoryGame
             int ChosenRow = 0, ChosenColumn = 0;
             bool Gamestop = false;
             int TrialCounter = 0;
-            int FirstNumToCheck = 0, SecondNumToCheck = 0;
-            int GamerPoints = 0;
+            int FirstNumToCheck = -1, SecondNumToCheck = -2;
+            int Gamer1Points = 0, Gamer2Points = 0;
+            string player = "Player 1";
 
             while (!Gamestop)
             {
@@ -32,12 +33,14 @@ namespace MemoryGame
                     FirstNumToCheck = 0;
                     SecondNumToCheck = 0;
                 }
+                Console.WriteLine($"This is {player} turn: ");
+                
                 do
                 {
-                    Console.WriteLine("Please write a row number:");
+                    Console.WriteLine("Please write a row number (zero based):");
                     ChosenRow = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Please write a column number:");
+                    Console.WriteLine("Please write a column number (zero based):");
                     ChosenColumn = Convert.ToInt32(Console.ReadLine());
 
                 } while ((ChosenRow < 0) && (ChosenColumn < 0) && (ChosenRow >= matrix.GetLength(0)) && (ChosenColumn >= matrix.GetLength(1)));
@@ -48,47 +51,92 @@ namespace MemoryGame
                 {
                     FirstNumToCheck = matrix[ChosenRow, ChosenColumn];
                 }
-
-                if (TrialCounter % 2 == 0)
+                else if (TrialCounter % 2 == 0)
                 {
                     SecondNumToCheck = matrix[ChosenRow, ChosenColumn];
                 }
-                //if the gamer has a point:
-                if (FirstNumToCheck == SecondNumToCheck)
-                {
-                    GamerPoints++;
-                    for (int i = 0; i < matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < matrix.GetLength(1); j++)
-                        {
-                            if (matrix[i, j] == FirstNumToCheck)
-                                matrix[i, j] = 0;
-                        }
-                    }
-
-                    Console.WriteLine("The new matrix now is:");
-                    for (int i = 0; i < matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < matrix.GetLength(1); j++)
-                        {
-                            if (matrix[i, j].ToString().Length == 1)
-                                Console.Write($"{ matrix[i, j]}  ");
-                            else
-                                Console.Write($"{ matrix[i, j]} ");
-                        }
-                        Console.WriteLine();
-                    }
-                }
+                
+                if (player == "Player 1")
+                    Gamer1Points = PartC(matrix, FirstNumToCheck, SecondNumToCheck, Gamer1Points, ChosenRow, ChosenColumn);
+                else if (player == "Player 2")
+                    Gamer2Points = PartC(matrix, FirstNumToCheck, SecondNumToCheck, Gamer2Points, ChosenRow, ChosenColumn);
 
                 TrialCounter++;
-                
+                if ((FirstNumToCheck != SecondNumToCheck) && (TrialCounter % 2 == 0))
+                {
+                    switchPlayer(ref player);
+                }
+
                 //Checking if to stop the game:
-                if (GamerPoints == (matrix.GetLength(0) * matrix.GetLength(1)) / 2)
+                if ((Gamer1Points + Gamer2Points) == (matrix.GetLength(0) * matrix.GetLength(1)) / 2)
                 {
                     Console.WriteLine($"The number of trials was: {TrialCounter / 2}");
-                    Gamestop = true;                    
+                    Gamestop = true;
                 }
-                    
+
+            }
+        }
+
+        private static void switchPlayer(ref string player)
+        {
+            if (player == "Player 1")
+                player = "Player 2";
+            else if (player == "Player 2")
+                player = "Player 1";
+        }
+
+        private static int PartC(int[,] matrix, int FirstNumToCheck, int SecondNumToCheck, int GamerPoints, int ChosenRow, int ChosenColumn)
+        {
+            //if the gamer has a point:
+            if (FirstNumToCheck == SecondNumToCheck)
+            {
+                GamerPoints++;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (matrix[i, j] == FirstNumToCheck)
+                            matrix[i, j] = 0;
+                    }
+                }
+
+                Console.WriteLine("The new matrix now is:");
+                PrintMatrixAsNumbers(matrix);
+                
+            }
+            PartD_PrintMatrixAsNumbers(matrix, ChosenRow, ChosenColumn);
+            return GamerPoints;
+        }
+
+        private static void PrintMatrixAsNumbers(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j].ToString().Length == 1)
+                        Console.Write($"{ matrix[i, j]}  ");
+                    else
+                        Console.Write($"{ matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void PartD_PrintMatrixAsNumbers(int[,] matrix, int ChosenRow, int ChosenColumn)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                        Console.Write($"{ matrix[i, j]} , ");
+                    else if ((i == ChosenRow) && (j == ChosenColumn))
+                        Console.Write($"{ matrix[i, j]} , ");
+                    else
+                        Console.Write($"X , ");
+                }
+                Console.WriteLine();
             }
         }
 
